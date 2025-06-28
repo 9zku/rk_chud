a6f1115a9c0e1d83bb350cb2df5bb074377c02b1
RK <rkfrmda3@gmail.com>
2025-06-28 13:57:32 -0500

    (refactor) thread, increase wait times, no longer wait for cache to start thread.

--- modules/threads/veh.lua
+++ modules/threads/veh.lua
@@ -5,64 +5,91 @@ local config = require('config')
 
 local vehThreads = {}
 local mainVehicleThread = nil
+local seatbeltControlThread = nil
 
 function vehThreads:startVehicleThread()
     if mainVehicleThread then return end 
     
     mainVehicleThread = CreateThread(function()
-        local lastControlCheck = 0
+        local wasInVehicle = false
         
-        while cache.vehicle do
-            local currentTime = GetGameTimer()
-            local vehicle = cache.vehicle
-
-            local speed = functions.GetVehSpped(vehicle)
-            local fuel = functions.GetFuel(vehicle)
-            local rpm = GetVehicleCurrentRpm(vehicle)
-            local health = functions.GetVehHealth(vehicle)
-            local seatbeltOn = seatbelt.GetbeltState()
+        while true do
+            local ped = PlayerPedId()
+            local isInVehicle = IsPedInAnyVehicle(ped, false)
             
-            local ped = cache.ped
-            local coords = GetEntityCoords(ped)
-            local streetName = GetStreetNameAtCoord(coords.x, coords.y, coords.z)
-            local streetNameString = GetStreetNameFromHashKey(streetName)
-            local location = GetLabelText(GetNameOfZone(coords.x, coords.y, coords.z))
-            
-            local heading = GetEntityHeading(ped)
-            local direction = functions.GetDirection(heading)
-            
-            local rpmPercent = rpm > 0.3 and (rpm - 0.3) / 0.7 * 100 or 0
-            
-            nui:message('setVehicleState', { 
-                inVehicle = true,
-                speed = math.floor(speed),
-                fuel = math.floor(fuel),
-                rpm = math.floor(rpmPercent),
-                health = health,
-                seatbeltOn = seatbeltOn,
-                streetName = streetNameString,
-                location = location,
-                direction = direction
-            })
-            
-            if currentTime - lastControlCheck >= 50 then
-                if seatbeltOn and config.DisableExitWithBelt then
-                    DisableControlAction(0, 23, true) 
-                    DisableControlAction(0, 47, true) 
-                    DisableControlAction(0, 58, true) 
-                    DisableControlAction(0, 75, true) 
+            if isInVehicle then
+                local vehicle = GetVehiclePedIsIn(ped, false)
+                
+                if DoesEntityExist(vehicle) then
+                    if not wasInVehicle then
+                        DisplayRadar(true)
+                        functions.ToggleMinimap()
+                        wasInVehicle = true
+                        self:startSeatbeltThread()
+                    end
+                    
+                    local speed = functions.GetVehSpped(vehicle)
+                    local fuel = functions.GetFuel(vehicle)
+                    local rpm = GetVehicleCurrentRpm(vehicle)
+                    local health = functions.GetVehHealth(vehicle)
+                    local seatbeltOn = seatbelt.GetbeltState()
+                    
+                    local coords = GetEntityCoords(ped)
+                    local streetName = GetStreetNameAtCoord(coords.x, coords.y, coords.z)
+                    local streetNameString = GetStreetNameFromHashKey(streetName)
+                    local location = GetLabelText(GetNameOfZone(coords.x, coords.y, coords.z))
+                    
+                    local heading = GetEntityHeading(ped)
+                    local direction = functions.GetDirection(heading)
+                    
+                    local rpmPercent = rpm > 0.3 and (rpm - 0.3) / 0.7 * 100 or 0
+                    
+                    nui:message('setVehicleState', { 
+                        inVehicle = true,
+                        speed = math.floor(speed),
+                        fuel = math.floor(fuel),
+                        rpm = math.floor(rpmPercent),
+                        health = health,
+                        seatbeltOn = seatbeltOn,
+                        streetName = streetNameString,
+                        location = location,
+                        direction = direction
+                    })
                 end
-                lastControlCheck = currentTime
+            else
+                if wasInVehicle then
+                    DisplayRadar(false)
+                    seatbelt:ResetBelt()
+                    wasInVehicle = false
+                    
+                    nui:message('setVehicleState', { 
+                        inVehicle = false,
+                    })
+                end
             end
             
-            Wait(450) 
+            Wait(350) 
         end
-        
-        nui:message('setVehicleState', { 
-            inVehicle = false,
-        })
-        
-        mainVehicleThread = nil
+    end)
+end
+
+function vehThreads:startSeatbeltThread()
+    if seatbeltControlThread then return end
+    
+    seatbeltControlThread = CreateThread(function()
+        while IsPedInAnyVehicle(PlayerPedId(), false) do
+            local seatbeltOn = seatbelt.GetbeltState()
+            
+            if seatbeltOn and config.DisableExitWithBelt then
+                DisableControlAction(0, 23, true) 
+                DisableControlAction(0, 47, true) 
+                DisableControlAction(0, 58, true) 
+                DisableControlAction(0, 75, true) 
+            end
+            
+            Wait(50)
+        end
+        seatbeltControlThread = nil
     end)
 end
 
