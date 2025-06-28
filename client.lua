d626ae7836fe1f2a6eaa12269e0557da360c16e2
RK <rkfrmda3@gmail.com>
2025-06-28 13:58:12 -0500

    (removal): Remove cache, remove unused requires.

--- client.lua
+++ client.lua
@@ -1,26 +1,11 @@
 local statusThreads = require('modules.threads.status')
 local vehThreads = require('modules.threads.veh')
 local miscThreads = require('modules.threads.misc')
-local utils = require('shared.utils')
-local functions = require('modules.functions')
-local seatbelt = require('modules.belt.seatbelt')
 local adjustments = require('modules.threads.adjustments')
 
 statusThreads:startStatusThread()
 miscThreads:startHideThread()
 miscThreads:HideMinimap()
 adjustments:Load()
-
-lib.onCache('vehicle', function(value, oldValue)
-    if value then
-        utils.Dbug('Player in vehicle, beginning vehicle thread - Sending vehicle state to NUI')
-        DisplayRadar(true)
-        functions.ToggleMinimap()
-        vehThreads:startVehicleThread()
-    else
-        utils.Dbug('Player exited vehicle, stopping vehicle thread - Sending vehicle state to NUI')
-        DisplayRadar(false)
-        seatbelt:ResetBelt()
-    end
-end)
+vehThreads:startVehicleThread()
 
