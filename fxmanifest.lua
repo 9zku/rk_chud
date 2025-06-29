143b527c3e7eee3389f56767df1d83793a22aceb
RK <rkfrmda3@gmail.com>
2025-06-28 20:14:49 -0500

    (chore): update version

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -4,7 +4,7 @@ game ( 'gta5' )
 author ({ 'RK ' })
 name ({ 'rk_chud' })
 description ({ 'Free and Open Source HUD' })
-version ({ '1.0.0' })
+version ({ '1.0.2' })
 
 ui_page ({ 'build/index.html' })
 
