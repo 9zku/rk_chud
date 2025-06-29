34354f9f89aa715ec9515c55476265640786e7e2
RK <rkfrmda3@gmail.com>
2025-06-28 19:56:19 -0500

    (refactor): change thread to use automatic inventory detection function.

--- modules/threads/misc.lua
+++ modules/threads/misc.lua
@@ -1,14 +1,17 @@
 local config = require('config')
 local nui = require('modules.nui.client')
+local utils = require('shared.utils')
 
 local miscThreads = {}
 
 function miscThreads:startHideThread()
     if not config.ShouldHudHide then return end
     
+    local inventoryStateBag = utils.getInventoryStateBag()
+    
     CreateThread(function()
         while true do
-            if IsPauseMenuActive() or LocalPlayer.state.invOpen then
+            if IsPauseMenuActive() or LocalPlayer.state[inventoryStateBag] then
                 nui:message('hideHud', {})
                 nui:message('hideCarHud', {})
             else
