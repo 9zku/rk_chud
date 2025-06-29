5cc389448f1489dc024379582612f786dea8dd9d
RK <rkfrmda3@gmail.com>
2025-06-28 20:14:28 -0500

    (feature): add multi-framework seatbelt system support

--- modules/belt/seatbelt.lua
+++ modules/belt/seatbelt.lua
@@ -2,11 +2,24 @@
 --- MAKE SURE YOU HAVE setr game_enableFlyThroughWindscreen true ADDED TO THE SERVER.CFG ---
 --------------------------------------------------------------------------------------------
 
+local config = require('config')
 local utils = require('shared.utils')
 local seatbelt = {}
 
 function seatbelt:Togglebelt()
     if not IsPedInAnyVehicle(PlayerPedId(), false) then return end
+
+    if config.seatbelt == 'qbx' then
+        return
+    end
+
+    if config.seatbelt == 'esx' then
+        return
+    end
+
+    if config.seatbelt == 'qb' then
+        return
+    end
 
     local currentBelt = LocalPlayer.state.seatbelt or false
     local newBelt = not currentBelt
@@ -26,17 +39,35 @@ function seatbelt:Togglebelt()
 end
 
 function seatbelt:ResetBelt()
-    LocalPlayer.state.seatbelt = false
+    if config.seatbelt ~= 'qbx' and config.seatbelt ~= 'esx' and config.seatbelt ~= 'qb' then
+        LocalPlayer.state.seatbelt = false
+    end
 end
 
-RegisterCommand('seatbelt', function()
-    seatbelt:Togglebelt()
-end, false)
+if config.seatbelt == 'default' then
+    RegisterCommand('seatbelt', function()
+        seatbelt:Togglebelt()
+    end, false)
 
-RegisterKeyMapping('seatbelt', 'Toggle Seatbelt', 'keyboard', 'B')
+    RegisterKeyMapping('seatbelt', 'Toggle Seatbelt', 'keyboard', 'B')
+end
 
 return {
-    GetbeltState = function() return LocalPlayer.state.seatbelt or false end,
+    GetbeltState = function() 
+        if config.seatbelt == 'qbx' then
+            return (LocalPlayer.state.seatbelt or false) or (LocalPlayer.state.harness or false)
+        end
+        
+        if config.seatbelt == 'esx' then
+            return exports['esx_cruisecontrol']:isSeatbeltOn()
+        end
+        
+        if config.seatbelt == 'qb' then
+            return exports['qb-smallresources']:HasSeatbeltOn() or exports['qb-smallresources']:HasHarness()
+        end
+        
+        return LocalPlayer.state.seatbelt or false 
+    end,
     Togglebelt = seatbelt.Togglebelt,
     ResetBelt = seatbelt.ResetBelt
 }
