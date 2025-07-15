01fe8f6c825ba4d66c9c6986ac6212556af679ed
RK <rkfrmda3@gmail.com>
2025-07-15 14:28:28 -0500

    lua beautifier

--- modules/threads/misc.lua
+++ modules/threads/misc.lua
@@ -1,44 +1,49 @@
-local config = require('config')
-local nui = require('modules.nui.client')
-local utils = require('shared.utils')
+local config = require("config")
+local nui = require("modules.nui.client")
+local utils = require("shared.utils")
 
 local miscThreads = {}
 
 ---@description: this was kinda done shitty and looks confusing.
 function miscThreads:startHideThread()
-    if not config.ShouldHudHide then return end
-    
+    if not config.ShouldHudHide then
+        return
+    end
+
     local inventoryStateBag = utils.getInventoryStateBag()
-    
-    CreateThread(function()
-        while true do
-            if IsPauseMenuActive() or LocalPlayer.state[inventoryStateBag] then
-                if hudEnabled then
-                    nui:message('hideHud', {}) --- Only hide the status hud if not already hidden by the user.
+
+    CreateThread(
+        function()
+            while true do
+                if IsPauseMenuActive() or LocalPlayer.state[inventoryStateBag] then
+                    if hudEnabled then
+                        nui:message("hideHud", {}) --- Only hide the status hud if not already hidden by the user.
+                    end
+                    nui:message("hideCarHud", {}) --- Hide carhud regardless.
+                else
+                    if hudEnabled then
+                        nui:message("showHud", {}) --- Show the status hud ONLY if it wasn't hidden by the user.
+                    end
+                    nui:message("showCarHud", {}) --- Always show the carhud.
                 end
-                nui:message('hideCarHud', {}) --- Hide carhud regardless.
-            else
-                if hudEnabled then
-                    nui:message('showHud', {}) --- Show the status hud ONLY if it wasn't hidden by the user.
-                end
-                nui:message('showCarHud', {}) --- Always show the carhud.
+                Wait(2000)
             end
-            Wait(2000)
         end
-    end)
+    )
 end
 
 function miscThreads:HideMinimap()
-    CreateThread(function()
-        while true do
-            if not cache.vehicle then
-                DisplayRadar(false)
-                SetRadarBigmapEnabled(false, false)
+    CreateThread(
+        function()
+            while true do
+                if not cache.vehicle then
+                    DisplayRadar(false)
+                    SetRadarBigmapEnabled(false, false)
+                end
+                Wait(2000)
             end
-            Wait(2000)
         end
-    end)
+    )
 end
-
 
 return miscThreads
