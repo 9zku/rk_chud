4ecac23b7581fd0011331749ca978c1c76a8b6db
RK <rkfrmda3@gmail.com>
2025-07-15 14:29:27 -0500

    lua beautifier

--- shared/utils.lua
+++ shared/utils.lua
@@ -1,35 +1,34 @@
-local config = require('config')
+local config = require("config")
 
 local utils = {}
 
----@param message string
 function utils.Dbug(message)
     if config.debug then
-        print('^3[DEBUG] ' .. message .. '^0')
+        print("^3[DEBUG] " .. message .. "^0")
     end
 end
 
----@param msg string
----@param type "inform" | "success" | "error" | "warning"
 function utils.notify(msg, type)
-    lib.notify({
-        title = 'HUD',
-        description = msg,
-        type = type
-    })
+    lib.notify(
+        {
+            title = "HUD",
+            description = msg,
+            type = type
+        }
+    )
 end
 
 ---@return "inv_busy" | "invOpen"
 function utils.getInventoryStateBag()
-    if GetResourceState('ox_inventory') == 'started' then
-        return 'invOpen'
+    if GetResourceState("ox_inventory") == "started" then
+        return "invOpen"
     end
 
-    if GetResourceState('qb-inventory') == 'started' then
-        return 'inv_busy'
+    if GetResourceState("qb-inventory") == "started" then
+        return "inv_busy"
     end
 
-    return 'invOpen'
+    return "invOpen"
 end
 
 return utils
