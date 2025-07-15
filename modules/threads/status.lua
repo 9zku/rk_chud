663646001931a86ceb547f7fcf68335acdc1fa52
RK <rkfrmda3@gmail.com>
2025-07-15 14:28:40 -0500

    lua beautifier

--- modules/threads/status.lua
+++ modules/threads/status.lua
@@ -1,23 +1,23 @@
-local config = require('config')
-local nui = require('modules.nui.client')
-local framework = require('modules.bridge.' .. config.framework)
+local config = require("config")
+local nui = require("modules.nui.client")
+local framework = require("modules.bridge." .. config.framework)
 local statusThreads = {}
 
 function statusThreads:status()
     if not framework.isPlayerLoaded() then
         return
     end
-    
+
     local ped = cache.ped
-    
-    local health = GetEntityHealth(ped) - 100 
+
+    local health = GetEntityHealth(ped) - 100
     local armour = GetPedArmour(ped)
     local stamina = math.floor(GetPlayerStamina(PlayerId()))
-    
+
     local frameworkStats = framework.getPlayerStatus() or {}
     local hunger = frameworkStats.hunger
     local thirst = frameworkStats.thirst
-    
+
     local status = {
         health = math.max(0, math.min(100, health)),
         armour = math.max(0, math.min(100, armour)),
@@ -25,17 +25,19 @@ function statusThreads:status()
         thirst = math.max(0, math.min(100, thirst)),
         stamina = math.max(0, math.min(100, stamina))
     }
-    
-    nui:message('updateStatus', status)
+
+    nui:message("updateStatus", status)
 end
 
 function statusThreads:startStatusThread()
-    CreateThread(function()
-        while true do
-            self:status()
-            Wait(800)
+    CreateThread(
+        function()
+            while true do
+                self:status()
+                Wait(800)
+            end
         end
-    end)
+    )
 end
 
 return statusThreads
