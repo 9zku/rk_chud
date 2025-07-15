7494d1a397b2bc8c5c63356f3006a86941701131
RK <rkfrmda3@gmail.com>
2025-07-15 14:26:23 -0500

    lua beautifier

--- modules/bridge/qbx.lua
+++ modules/bridge/qbx.lua
@@ -1,35 +1,40 @@
-local config = require('config')
-local utils = require('shared.utils')
+local config = require("config")
+local utils = require("shared.utils")
 
-if config.framework ~= 'qbx' then return end
+if config.framework ~= "qbx" then
+    return
+end
 
 local framework = {}
 local playerLoaded = false
 
-RegisterNetEvent('QBCore:Client:OnPlayerLoaded', function()
-	playerLoaded = true
-	utils.Dbug('Player Successfully Loaded - Toggling hud on.')
-end)
+RegisterNetEvent(
+    "QBCore:Client:OnPlayerLoaded",
+    function()
+        playerLoaded = true
+        utils.Dbug("Player Successfully Loaded - Toggling hud on.")
+    end
+)
 
 function framework.getPlayerStatus()
-	local player = exports.qbx_core:GetPlayerData()
+    local player = exports.qbx_core:GetPlayerData()
 
-	if player and player.metadata then
-		return {
-			hunger = math.floor(player.metadata.hunger),
-			thirst = math.floor(player.metadata.thirst),
-		}
-	end
+    if player and player.metadata then
+        return {
+            hunger = math.floor(player.metadata.hunger),
+            thirst = math.floor(player.metadata.thirst)
+        }
+    end
 
-	return {
-		hunger = 0,
-		thirst = 0,
-	}
+    return {
+        hunger = 0,
+        thirst = 0
+    }
 end
 
 ---@return boolean
 function framework.isPlayerLoaded()
-	return playerLoaded
+    return playerLoaded
 end
 
 return framework
