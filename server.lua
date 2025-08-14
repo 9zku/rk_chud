c3f2ee00a760657589f709412c964b2b464d709c
RK <rkfrmda3@gmail.com>
2025-08-14 12:01:53 -0500

    Update server.lua

--- server.lua
+++ server.lua
@@ -5,9 +5,7 @@ local ResourceName = GetResourceMetadata(GetCurrentResourceName(), 'name', 0)
 lib.versionCheck(("rk3gaming/%s"):format(ResourceName))
 
 AddEventHandler('onResourceStart', function(resourceName)
-    if GetCurrentResourceName() ~= resourceName then return end
-
-    if GetCurrentResourceName() ~= ResourceName then
+    if resourceName == ResourceName and ResourceName ~= 'rk_chud' then
         print('^1[ERROR] This resource must be named rk_chud^7')
         Wait(10000)
     end
