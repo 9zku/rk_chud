4427955839464a627eb931013282613d9197f930
RK <rkfrmda3@gmail.com>
2025-08-14 16:29:26 -0500

    (fix): update name

--- client.lua
+++ client.lua
@@ -1,6 +1,6 @@
 local statusThreads = require('modules.threads.status')
 local vehThreads = require('modules.threads.veh')
-local miscellaneousThreads = require('modules.threads.misc')
+local miscellaneousThreads = require('modules.threads.miscellaneous')
 local adjustments = require('modules.threads.adjustments')
 
 adjustments:Load()
